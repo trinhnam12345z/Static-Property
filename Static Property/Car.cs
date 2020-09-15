035f162b9208cb132d0431e8c676a88d6eeb618a
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-15 22:42:49 +0700

    Initial commit

---
+++ Static Property/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Property
+{
+    public class Car
+    {
+
+        private string name;
+        private string engine;
+        public static int numberOfCars;
+        public Car(string name, string engine)
+        {
+            this.name = name;
+            this.engine = engine;
+            numberOfCars++;
+        }
+        public void setName(string newName)
+        {
+            this.name = newName;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public string getEngine()
+        {
+            return engine;
+        }
+        public void setEngine(string newEngine)
+        {
+            this.engine = newEngine;
+        }
+    }
+}
